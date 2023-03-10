57654e8d7577208ad2da5814cd113d5b954e3c63
Jhonas Alves <jhonasma@icloud.com>
2023-03-10 16:10:41 -0300

    adiciona listeners e funcoes num e clear

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const btnOperador = document.querySelectorAll(".btn-operador");
 const btnClear = document.querySelector(".btn-clear");
 const btnDelete = document.querySelector(".btn-delete");
 const btnIgual = document.querySelector(".btn-igual");
+const anterior = document.querySelector(".anterior");
+const atual = document.querySelector(".atual");
+
+function addNum(event) {
+  atualizaVisor(event.currentTarget.innerText);
+}
+
+function limpaVisor() {
+  atual.innerText = "";
+}
+
+function atualizaVisor(valor) {
+  atual.innerText += valor;
+}
+btnNum.forEach((numero) => {
+  numero.addEventListener("click", addNum);
+});
+
+btnClear.addEventListener("click", limpaVisor);
