50cfecd2153de4f6ad25e943d3240bd6d90a2e55
Jhonas Alves <jhonasma@icloud.com>
2023-03-10 18:11:36 -0300

    adiciona evento onload e favicon

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
-// window.onload;
+window.onload = function () {
+  function initCalc() {
+    const btnNum = document.querySelectorAll(".btn-num");
+    const btnOperador = document.querySelectorAll(".btn-operador");
+    const btnClear = document.querySelector(".btn-clear");
+    const btnDelete = document.querySelector(".btn-delete");
+    const btnIgual = document.querySelector(".btn-igual");
+    const anterior = document.querySelector(".anterior");
+    const atual = document.querySelector(".atual");
 
-const btnNum = document.querySelectorAll(".btn-num");
-const btnOperador = document.querySelectorAll(".btn-operador");
-const btnClear = document.querySelector(".btn-clear");
-const btnDelete = document.querySelector(".btn-delete");
-const btnIgual = document.querySelector(".btn-igual");
-const anterior = document.querySelector(".anterior");
-const atual = document.querySelector(".atual");
+    function addNum(event) {
+      atualizaVisor(event.currentTarget.innerText);
+    }
 
-function addNum(event) {
-  atualizaVisor(event.currentTarget.innerText);
-}
+    function addOprtr(event) {
+      if (event.currentTarget.innerText === "x") {
+        atualizaVisor("*");
+      } else if (event.currentTarget.innerText === "÷") {
+        atualizaVisor("/");
+      } else {
+        atualizaVisor(event.currentTarget.innerText);
+      }
+    }
 
-function addOprtr(event) {
-  if (event.currentTarget.innerText === "x") {
-    atualizaVisor("*");
-  } else if (event.currentTarget.innerText === "÷") {
-    atualizaVisor("/");
-  } else {
-    atualizaVisor(event.currentTarget.innerText);
+    function realizarOperacao() {
+      atual.innerText = eval(atual.innerText);
+    }
+
+    function limpaVisor() {
+      atual.innerText = "";
+    }
+
+    function delUltimoValor() {
+      atual.innerText = atual.innerText.substring(
+        0,
+        atual.innerText.length - 1
+      );
+    }
+
+    function atualizaVisor(valor) {
+      atual.innerText += valor;
+    }
+
+    btnNum.forEach((numero) => {
+      numero.addEventListener("click", addNum);
+    });
+
+    btnOperador.forEach((operador) => {
+      operador.addEventListener("click", addOprtr);
+    });
+
+    btnClear.addEventListener("click", limpaVisor);
+
+    btnDelete.addEventListener("click", delUltimoValor);
+
+    btnIgual.addEventListener("click", realizarOperacao);
   }
-}
 
-function realizarOperacao() {
-  atual.innerText = eval(atual.innerText);
-}
-
-function limpaVisor() {
-  atual.innerText = "";
-}
-
-function delUltimoValor() {
-  atual.innerText = atual.innerText.substring(0, atual.innerText.length - 1);
-}
-
-function atualizaVisor(valor) {
-  atual.innerText += valor;
-}
-
-btnNum.forEach((numero) => {
-  numero.addEventListener("click", addNum);
-});
-
-btnOperador.forEach((operador) => {
-  operador.addEventListener("click", addOprtr);
-});
-
-btnClear.addEventListener("click", limpaVisor);
-
-btnDelete.addEventListener("click", delUltimoValor);
-
-btnIgual.addEventListener("click", realizarOperacao);
+  initCalc();
+};
