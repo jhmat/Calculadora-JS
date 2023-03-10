b76c7b642fc98cf25e555ec24185436535442b4f
Jhonas Alves <jhonasma@icloud.com>
2023-03-10 15:03:59 -0300

    altera seletores

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 // window.onload;
 
-const buttons = document.querySelectorAll("button");
-const visor = document.querySelector(".visor");
-
-buttons.forEach((element) => {
-  element.addEventListener("click", adicionaNumero);
-});
-
-function adicionaNumero() {
-  let numero = this.innerText;
-}
+const btnNum = document.querySelectorAll(".btn-num");
+const btnOperador = document.querySelectorAll(".btn-operador");
+const btnClear = document.querySelector(".btn-clear");
+const btnDelete = document.querySelector(".btn-delete");
+const btnIgual = document.querySelector(".btn-igual");
